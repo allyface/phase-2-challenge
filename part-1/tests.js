df06523fb55ec1043f77ddb381f221bb51cc5e1a
Ally Kendall <ally.b.kendall@gmail.com>
2017-06-21 04:58:22 -0700

    Completes numProps with all tests written and passing

--- part-1/tests.js
+++ part-1/tests.js
@@ -2,10 +2,6 @@ import {expect} from 'chai'
 import Functions from '../part-1/functions.js'
 
 describe('Functions.weekday()', () => {
-  it('should be a function', function() {
-    expect(Functions.weekday).to.be.a('function')
-  })
-
   it('should return day of week for given date', () => {
     expect(Functions.weekday(new Date(2017, 5, 15))).to.equal('Thu')
   })
@@ -15,11 +11,7 @@ describe('Functions.weekday()', () => {
   })
 })
 
-describe.only('Functions.snippet()', () => {
-  it('should be a function', function() {
-    expect(Functions.snippet).to.be.a('function')
-  })
-
+describe('Functions.snippet()', () => {
   it('should return a string shortened to the maxLength', () => {
     expect(Functions.snippet('For the following exercises,', 10)).to.equal('For the fo…')
   })
@@ -30,5 +22,21 @@ describe.only('Functions.snippet()', () => {
 
   it('should throw an error when given invalid input', () => {
     expect(Functions.snippet.bind(Functions, 'Hi', 'a')).to.throw(Error, 'Not a valid input')
+  })
+})
+
+describe.only('Functions.numProps()', () => {
+  let friend = {
+  name: 'Dominique',
+  age: 30,
+  phone: '555-555-5555'
+  }
+
+  it('should return the number of properties in an object', () => {
+    expect(Functions.numProps(friend)).to.equal(3)
+  })
+
+  it('should throw an error when given invalid input', () => {
+    expect(Functions.numProps.bind(Functions, 5)).to.throw(Error, 'Not a valid input')
   })
 })
